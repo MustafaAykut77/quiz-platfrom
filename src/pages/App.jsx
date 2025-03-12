40fd4ad1169ddd3676da356db62ffa911c5fb7d4
Mustafa Aykut <mustafaykut77@gmail.com>
2025-03-13 00:21:14 +0300

    React Application Tests

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,25 +1,15 @@
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import SideBar from '../Components/SideBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-green-700">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex text-green-700 font-bold">
+        <SideBar/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: 'jit',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
