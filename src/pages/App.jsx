683ecca317545fccf0c65aec4adcf6ea6e617a9b
Eren GURELI <erengureli10@gmail.com>
2025-04-13 21:47:44 +0300

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import SideBar from '../Components/SideBar'
+import SideBar from '../components/SideBar'
 
 function App() {
   const [count, setCount] = useState(0)
