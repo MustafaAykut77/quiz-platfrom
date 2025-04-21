76a5c173bd44dd0402a9f0ef4c9bb322f62693ed
Eren Köse <erenkosee471@gmail.com>
2025-04-21 23:17:41 +0300

    shadcn kuruldu.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import './App.css'
 import SideBar from '../components/SideBar'
 
+// Component Imports
+import { Button } from "@/components/ui/button"
+import { Progress } from "@/components/ui/progress"
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -9,6 +13,8 @@ function App() {
     <>
       <div className="flex text-green-700 font-bold">
         <SideBar/>
+        <Button variant="outline">Button</Button>
+        <Progress value={33} />
       </div>
     </>
   )
