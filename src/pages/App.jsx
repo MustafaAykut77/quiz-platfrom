9398fafc1a02c1bf22f6daa569e934e826af96a9
Eren Köse <erenkosee471@gmail.com>
2025-04-22 16:54:35 +0300

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -12,7 +12,6 @@ export default function App() {
   return (
     <>
       <div className="flex w-full max-w-sm items-center space-x-2 font-bold">
-        <SideBar/>
 
         <Button variant="destructive" className={"absolute right-5 top-5 text-amber-100 h-15 w-15"} type="submit">
           <Avatar>
