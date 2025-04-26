28ca854dc3bf88ec5ea3d0761538b137fd8e4db0
Eren Köse <erenkosee471@gmail.com>
2025-04-26 18:52:26 +0300

    Socket.IO connections added.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,21 @@
 import express from "express";
 const app = express();
 const port = 3000;
+import { Server } from 'socket.io';
+const io = new Server(port, {
+  cors:{
+    origin: ["http://localhost:5173/"],
+  }
+});
+
+
+io.on("connection", socket => {
+    console.log(socket.id);
+    socket.on("send-code", (code) => {
+    console.log(code); 
+    })
+  })
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- src/pages/quiz/quiz.jsx
+++ src/pages/quiz/quiz.jsx
@@ -1,7 +1,53 @@
+import { io } from "socket.io-client";
+import React, { useState } from 'react';
+
+// Component Imports
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const socket = io("http://localhost:3000");
+
 const Quiz = () => {
-    return ( 
-        <div>Quiz Page</div>
-     );
+  const [code, setCode] = useState('');
+
+  const handleInputChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  const handleJoinQuiz = () => {
+    socket.emit("send-code", code);
+    console.log("Gönderilen Kod:", code);
+  };
+
+  return (
+    <>
+      <div className="flex w-full max-w-sm items-center space-x-2 font-bold">
+
+        <Button variant="destructive" className={"absolute right-5 top-20 text-amber-100 h-15 w-15 "} type="button">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </Button>
+
+        <Input
+          className={"text-amber-100"}
+          placeholder="Quiz Kodunu Girin"
+          value={code}
+          onChange={handleInputChange}
+        />
+        <Button
+          variant="destructive"
+          className={"text-amber-100 h-13 w-15"}
+          type="button"
+          onClick={handleJoinQuiz}
+        >
+          Gir
+        </Button>
+      </div>
+    </>
+  );
 }
- 
+
 export default Quiz;
