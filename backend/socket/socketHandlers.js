9d1e59d7fbc02c61a154e2ab6725f42a656250b5
Eren GURELI <erengureli10@gmail.com>
2025-06-12 19:54:11 +0300

    Update socket

--- backend/socket/socketHandlers.js
+++ backend/socket/socketHandlers.js
@@ -1,21 +1,29 @@
-
+import { addPlayer, updatePlayer } from "../services/gameService.js";
+import { authToken } from "../middleware/authToken.js";
 
 export const setupSocketHandlers = (io) => {
 	io.on("connection", (socket) => {
         console.log("A user connected:", socket.id);
 
-        socket.on("join_game", (code) => {
-            console.log(`User ${socket.id} joined game ${code}`);
+        socket.on("join_game", (code, username) => {
+            const result = addPlayer(code, username);
+            if (!result.success) {
+                console.error(`Error adding player: ${result.error}`);
+                io.to(socket.id).emit('join_return', { error: result.error });
+                return;
+            }
+
             socket.join(code);
-
-            io.to(code).emit('user_joined');
-        });
-
-        socket.on("set_username", (username) => {
-            console.log(`User ${socket.id} set username: ${username}`);
+            socket.room = code;
             socket.username = username;
 
-            io.to(socket.id).emit('username_set', { username });
-        });
+            console.log(`User ${socket.id} | ${username} joined game ${code}`);
+
+            io.to(code).emit('join_return', { success: true, code, username });
+        });        
+    });
+
+    io.of("/admin").use(authToken).on("connection", (socket) => {
+        console.log("An admin connected:", socket.user);
     });
 };
