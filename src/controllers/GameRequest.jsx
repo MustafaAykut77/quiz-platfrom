96b52f18a074907efcbe1e94b91264f0573eddd5
Mustafa Aykut <mustafaykut77@gmail.com>
2025-06-13 01:41:42 +0300

    Add startGame and getGameData

--- src/controllers/GameRequest.jsx
+++ src/controllers/GameRequest.jsx
@@ -13,7 +13,7 @@ const createGame = async (token, quizid) => {
     return response.data;
   } catch (error) {
     console.error('Error creating game:', error.response?.data || error.message);
-    throw new Error(error.response?.data?.message || 'Failed to create game');
+    return "Hata oluştu, lütfen tekrar deneyin.";
   }
 };
 
@@ -33,4 +33,36 @@ const getGame = async (gameid) => {
   }
 };
 
-export { createGame, getGame };
+const startGame = async (token) => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/game/start`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error starting game:', error.response?.data || error.message);
+    return "Hata oluştu, lütfen tekrar deneyin.";
+  }
+}
+
+const getGameData = async (token) => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/game/get`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching game data:', error.response?.data || error.message);
+    return "Hata oluştu, lütfen tekrar deneyin.";
+  }
+}
+
+export { createGame, getGame, startGame, getGameData };
