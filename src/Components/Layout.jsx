a09120c0d74a8a491444749caad2a1897e8ba2fc
Mustafa Aykut <mustafaykut77@gmail.com>
2025-06-13 23:57:42 +0300

    Remove Sidebar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,79 +1,18 @@
-// Shadcn Sidebar imports
-import { 
-  Sidebar, 
-  SidebarContent, 
-  SidebarHeader, 
-  SidebarMenu, 
-  SidebarMenuItem, 
-  SidebarMenuButton,
-  SidebarProvider,
-} from "@/components/ui/sidebar";
-
 import Header from "./Header";
 import Footer from "./Footer";
-import { SidebarTrigger } from '@/components/ui/sidebar'
 
 // Layout component with sidebar
 const Layout = ({ children }) => {
   return (
-    <SidebarProvider>
-      <div className="flex h-screen">
-        <Sidebar collapsible="icon" className="mt-14">
-          <SidebarContent>
-            <SidebarMenu>
-              <SidebarMenuItem className={"flex justify-left ml-1 mt-2"}> 
-                <SidebarTrigger/>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="/">
-                    <span>Ana Sayfa</span>
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="/home">
-                    <span>Quiz</span>
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="/createquiz">
-                    <span>Quiz Oluştur</span>
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="/login">
-                    <span>Giriş Yap</span>
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="/register">
-                    <span>Kayıt Ol</span>
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarContent>
-        </Sidebar>
-        
-        <div className="flex-1 flex flex-col">   
-          <Header />
-          
-          <main className="flex-1 mt-14">
-            {children}
-          </main>
-          
-          <Footer />
-        </div>
-      </div>
-    </SidebarProvider>
+    <div className="flex-1 flex flex-col">   
+      <Header />
+      
+      <main className="flex-1 mt-14">
+        {children}
+      </main>
+      
+      <Footer />
+    </div>
   );
 };
 
